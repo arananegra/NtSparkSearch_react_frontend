988d0a06edb8b085ac464db7b3f8aa87adc5f03b
Alvaro Gomez <arananegrayeye@gmail.com>
2017-07-10 17:04:17 +0200

    Prod build process

--- pro-compilation.js
+++ pro-compilation.js
@@ -1,7 +1,122 @@
-/**
- * Created with JetBrains WebStorm.
- * User: alicia
- * Date: 20/04/17
- * Time: 11:16
- * To change this template use File | Settings | File Templates.
- */
+let path = require('path');
+let webpack = require("webpack");
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let basePath = __dirname;
+
+module.exports = function(env) {
+    return {
+        context: path.join(basePath, 'src'),
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+
+        target: "web",
+
+        node: {
+            fs: "empty",
+            net: "empty"
+        },
+        entry: {
+            styles: './scss/index.scss',
+            app: [
+                '../index.tsx'
+            ],
+            vendorStyles: [
+                '../node_modules/bootstrap/dist/css/bootstrap.css'
+            ]
+        },
+        output: {
+            path: path.join(basePath, './dist'),
+            filename: '[hash].[name].js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'awesome-typescript-loader'
+                    }]
+                },
+                {
+                    test: /\.(js)$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+                {
+                    test: /\.(html)$/,
+                    exclude: /node_modules/,
+                    loader: 'html-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: /node_modules/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            { loader: 'css-loader', },
+                            { loader: 'sass-loader', },
+                        ],
+                    })
+                },
+                {
+                    test: /\.css$/,
+                    include: /node_modules/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: {
+                            loader: 'css-loader',
+                        },
+                    })
+                },
+                {
+                    test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+                },
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+                },
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+                },
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production')
+                }
+            }),
+            // new webpack.optimize.UglifyJsPlugin({
+            //     compress:{
+            //         warnings: true
+            //     }
+            // }),
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest'],
+            }),
+            new ExtractTextPlugin({
+                filename: '[hash].[name].css',
+                disable: false,
+                allChunks: true,
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html', //Name of file in ./dist/
+                template: 'index.html', //Name of template in ./src
+                hash: true
+            }),
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery"
+            }),
+        ]
+    }
+};
